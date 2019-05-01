c6dc2e9af0c660c2f878cd51530bdd449fa7f85b
Amaan Iqbal <amaaniqbal2786@gmail.com>
2019-05-01 18:14:08 +0530

    Identify instructions

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ struct Car {
     string color;
 };
 
+//This Class Stores all the information about the Parking Lot
 class ParkingLot {
     int numberOfCarsAllowed;
     int numberOfCarsParked; 
@@ -23,12 +24,57 @@ public :
     //Helper Functions Here
 };
 
+int getNumberOfSlotsFromInput(string input) {
+    int n = stoi(input.substr(19));
+    return n;
+}
+
+string getInstructionFromInput(string input) {
+    string result = "";
+    for(int i=0; i<input.length(); i++) {
+        if(input[i] == ' ')
+            break;
+        result += input[i];
+    }
+    return result;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //Input and Output goes here
+    //Stores individual lines of the Input 
+    string input;
+    
+    getline(cin, input);
+    int numberOfSlots = getNumberOfSlotsFromInput(input);
+
+    //Create Object of ParkingLot Class
+    ParkingLot P(numberOfSlots);
+
+    do {
+        getline(cin, input);
+        string instruction = getInstructionFromInput(input);
+
+        if(instruction.compare("park") == 0) {
+            //Action
+        } else if(instruction.compare("status") == 0) {
+            //Action
+        } else if(instruction.compare("leave") == 0) {
+            //Action
+        } else if(instruction.compare("registration_numbers_for_cars_with_colour") == 0) {
+            //Action
+        } else if(instruction.compare("slot_numbers_for_cars_with_colour") == 0) {
+            //Action
+        } else if(instruction.compare("slot_number_for_registration_number") == 0) {
+
+        } else if(instruction.compare("exit") == 0) {
+            break;
+        } else {
+            cout << "\nSorry Your Action Cannot Be Identified!!!\n";
+        }
+    } while(instruction[0] != '\n');
+
 
     return 0;
 }
