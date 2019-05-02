2d0f904699ad37cc7ea88689e0d248aa9edc2453
Amaan Iqbal <amaaniqbal2786@gmail.com>
2019-05-02 23:18:00 +0530

    Add Unit Test for getRegistrationNumbersByColor()

--- unit_tests/Test.cpp
+++ unit_tests/Test.cpp
@@ -51,6 +51,31 @@ TEST(testGetSlotNumberByRegistrationNumber, whenCarAbsent) {
     cout << "Unit Test Successful for getSlotNumberByRegistrationNumber function, when searched car was absent!\n";
 }
 
+TEST(testGetRegistrationNumbersByColor, whenColorPresent) {
+    ParkingLot P(3);
+    P.addCar("KA-01-HH-1234", "White");
+    P.addCar("KA-01-HH-9999", "White");
+    P.addCar("KA-01-BB-0001", "Black");
+    
+    //Get Registration Numbers
+    vector<string> regNumber = P.getRegistrationNumbersByColor("White");
+    ASSERT_EQ(regNumber[0], "KA-01-HH-1234");
+    ASSERT_EQ(regNumber[1], "KA-01-HH-9999");
+    cout << "Unit Test Successful for getRegistrationNumbersByColor function, when searched color was present!\n";
+}
+
+TEST(testGetRegistrationNumbersByColor, whenColorAbsent) {
+    ParkingLot P(3);
+    P.addCar("KA-01-HH-1234", "White");
+    P.addCar("KA-01-HH-9999", "White");
+    P.addCar("KA-01-BB-0001", "Black");
+    
+    //Get Registration Numbers
+    vector<string> regNumber = P.getRegistrationNumbersByColor("Red");
+    ASSERT_EQ(regNumber.size(), 0);
+    cout << "Unit Test Successful for getRegistrationNumbersByColor function, when searched color was absent!\n";
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest( &argc, argv );
     return RUN_ALL_TESTS();
